2c10b0f31887c0a40c9ba366f220baf8ca625f74
akash-bokefod <bokefodakash@gmail.com>
2026-02-23 16:50:01 +0530

    add a home page route

--- index.js
+++ index.js
@@ -72,6 +72,10 @@ app.use('/listings',listingRouter);
 app.use('/listings/:id/reviews',reviewRouter);
 app.use('/',userRouter);
 
+app.use('/',(req,res)=>{
+    res.redirect("/listings");
+})
+
 app.use((req,res,next)=>{
     next(new ExpressError(404,"Page Not Found"));
 })
